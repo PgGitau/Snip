22d68cadfb64a24a16fc0daa697b4d84a4f74b4e
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-05-20 22:55:27 +0300

    add js file for navbar burger clicks

---
+++ resources/nav.js
@@ -0,0 +1,14 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.burger');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    /*console.log(visibility) should return false bcs that's how its set in the html*/
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible', true); /*in the brackets its ('attribute', what value you set)*/  
+        navToggle.setAttribute('aria-expanded', true); /*for s-r aria-expanded becomes true bcs the menu is expanded*/
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+})
