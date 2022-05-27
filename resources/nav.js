cdf3236a4ba1fbfbdde5a4477238123fc582e78a
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-05-27 22:38:52 +0300

    add JS to minimize transition when resizing

--- resources/nav.js
+++ resources/nav.js
@@ -17,6 +17,17 @@ navToggle.addEventListener('click', () => {
     }
 })
 
+/*to minimise transition when resizing screen [its not really necessary]*/
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.   body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+
 
 
 
